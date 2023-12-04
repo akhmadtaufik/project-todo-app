9fef5980752a48a278d4b0566f8340a1c0e68c74
akhmadtaufik <akhmadtaufik1703@gmail.com>
2023-12-04 10:41:08 +0700

    feat: Implement Add Task functionality in homepage

--- app/static/assets/js/script-home.js
+++ app/static/assets/js/script-home.js
@@ -13,3 +13,50 @@
     $("#sidebar").toggleClass("active");
   });
 })(jQuery);
+
+// Fungsi Add Task
+document.addEventListener("DOMContentLoaded", function () {
+  const modalAdd = new bootstrap.Modal(document.getElementById("modalAdd"));
+  const btnAdd = document.getElementById("btn-add");
+  const btnAddTask = document.querySelector("#modalAdd button.btn-primary");
+  const titleInput = document.getElementById("recipient-name");
+  const descriptionInput = document.getElementById("message-text");
+  const todoColumn = document.getElementById("todo");
+
+  btnAdd.addEventListener("click", function () {
+    modalAdd.show();
+  });
+
+  btnAddTask.addEventListener("click", function () {
+    const title = titleInput.value.trim();
+    const description = descriptionInput.value.trim();
+
+    if (title !== "" && description !== "") {
+      // Buat elemen artikel baru untuk tugas
+      const newTask = document.createElement("article");
+      newTask.classList.add("border", "p-3", "drag");
+      newTask.innerHTML = `
+        <h4>${title}</h4>
+        <p>${description}</p>
+        <button class="badge bg-danger" onclick="deleteTask(this)">Delete</button>
+        <button class="badge bg-info" onclick="editTask(this)">Edit</button>
+      `;
+
+      // Tambahkan tugas baru ke kolom "Todo"
+      todoColumn.appendChild(newTask);
+
+      // Kosongkan input setelah menambahkan tugas
+      titleInput.value = "";
+      descriptionInput.value = "";
+
+      // Tutup modal setelah menambahkan tugas
+      modalAdd.hide();
+    }
+  });
+});
+
+function deleteTask(button) {
+  // Hapus elemen artikel (tugas) saat tombol "Delete" diklik
+  const task = button.parentElement;
+  task.remove();
+}
