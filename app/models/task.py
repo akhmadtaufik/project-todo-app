df255723b7e615ca9fc5e7b0729befc22c9118dd
akhmadtaufik <akhmadtaufik1703@gmail.com>
2023-12-15 15:21:59 +0700

    feat: Introduce Tasks model with SQLAlchemy

---
+++ app/models/task.py
@@ -0,0 +1,29 @@
+from app.extensions import db
+
+
+class Tasks(db.Model):
+    __tablename__ = "tasks"
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.Text)
+    due_date = db.Column(db.DATE)
+    status = db.Column(db.String(50))
+    project_id = db.Column(
+        db.Integer, db.ForeignKey("projects.id", ondelete="CASCADE"), nullable=False
+    )
+    created_at = db.Column(db.TIMESTAMP, server_default=db.func.now())
+    update_at = db.Column(
+        db.TIMESTAMP, server_default=db.func.now(), onupdate=db.func.now()
+    )
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "task_name": self.task_name,
+            "description": self.description,
+            "due_date": self.due_date,
+            "status": self.status,
+            "project_id": self.project_id,
+            "created_at": self.created_at,
+            "update_at": self.update_at,
+        }
