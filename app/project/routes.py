ce79818fdbad75381215cf5c34b32958e2f0bfc6
akhmadtaufik <akhmadtaufik1703@gmail.com>
2023-12-16 15:29:53 +0700

    feat: Introduce project creation endpoint

--- app/project/routes.py
+++ app/project/routes.py
@@ -18,3 +18,31 @@ def get_all_project():
     response = jsonify({"success": True, "data": results})
 
     return response, 200
+
+
+@projectBP.route("/", methods=["POST"], strict_slashes=False)
+def cretae_project():
+    data = request.get_json()
+    input_project = data.get("project_name")
+    input_description = data.get("description")
+    input_user_id = data.get("user_id")
+
+    if not input_project or not input_description or not input_user_id:
+        return jsonify({"message": "Invalid Parameter"})
+
+    new_project = Projects(
+        project_name=input_project, description=input_description, user_id=input_user_id
+    )  # type: ignore
+
+    db.session.add(new_project)
+    db.session.commit()
+
+    response = jsonify(
+        {
+            "success": True,
+            "data": new_project.serialize(),
+            "message": "Project successfully created",
+        }
+    )
+
+    return response, 201
