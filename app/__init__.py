f0704a7a160f051cd46560faf00241ee6078df74
akhmadtaufik <akhmadtaufik1703@gmail.com>
2023-12-16 13:20:55 +0700

    feat: Extend app functionality with new blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from app.extensions import db, migrate
+from app.project import projectBP
+from app.task import taskBP
 from app.user import userBP
 from config import Config
 from flask import Flask
@@ -12,5 +14,7 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
 
     app.register_blueprint(userBP, url_prefix="/users")
+    app.register_blueprint(projectBP, url_prefix="/projects")
+    app.register_blueprint(taskBP, url_prefix="/projects/task")
 
     return app
