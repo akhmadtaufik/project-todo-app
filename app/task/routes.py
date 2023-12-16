a4485f52ce9a4ea3e6c0a1d6f3bb696e0a0407cb
akhmadtaufik <akhmadtaufik1703@gmail.com>
2023-12-16 15:31:05 +0700

    feat: Introduce task creation endpoint

--- app/task/routes.py
+++ app/task/routes.py
@@ -20,3 +20,43 @@ def get_all_task():
     response = jsonify({"success": True, "data": results})
 
     return response, 200
+
+
+@taskBP.route("/", methods=["POST"], strict_slashes=False)
+def cretae_task():
+    data = request.get_json()
+    input_task = data.get("task_name")
+    input_description = data.get("description")
+    input_due_date = data.get("due_date")
+    input_status = data.get("status")
+    input_project_id = data.get("project_id")
+
+    if (
+        not input_task
+        or not input_description
+        or not input_due_date
+        or not input_status
+        or not input_project_id
+    ):
+        return jsonify({"message": "Invalid parameter"}), 422
+
+    new_task = Tasks(
+        task_name=input_task,
+        description=input_description,
+        due_date=input_due_date,
+        status=input_status,
+        project_id=input_project_id,
+    )  # type: ignore
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    response = jsonify(
+        {
+            "success": True,
+            "data": new_task.serialize(),
+            "message": "Task successfully created",
+        }
+    )
+
+    return response, 201
