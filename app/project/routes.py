567a5b0d9e82dfac6f31119883bb5975685d1382
akhmadtaufik <akhmadtaufik1703@gmail.com>
2023-12-17 12:53:52 +0700

    feat: Add additional project CRUD operations

--- app/project/routes.py
+++ app/project/routes.py
@@ -46,3 +46,64 @@ def cretae_project():
     )
 
     return response, 201
+
+
+@projectBP.route("/<int:id>", methods=["GET"], strict_slashes=False)
+def get_project_by_id(id):
+    project = Projects.query.filter_by(id=id).first()
+
+    if not project:
+        return jsonify({"success": False, "message": "Project not found"}), 404
+
+    else:
+        response = jsonify({"success": True, "data": project.serialize()})
+
+    return response, 200
+
+
+@projectBP.route("/<int:id>", methods=["PUT"], strict_slashes=False)
+def update_project(id):
+    data = request.get_json()
+    input_project = data.get("project_name")
+    input_description = data.get("description")
+    input_user_id = data.get("user_id")
+
+    project = Projects.query.filter_by(id=id).first()
+
+    if not project:
+        return jsonify({"success": False, "message": "Project not found"}), 404
+
+    elif not input_project or not input_description or not input_user_id:
+        return jsonify({"success": False, "message": "Data not complete"}), 422
+
+    else:
+        project.project_name = input_project
+        project.description = input_description
+        project.user_id = input_user_id
+
+    db.session.commit()
+
+    response = jsonify(
+        {
+            "success": True,
+            "message": "Project successfully updated",
+            "data": project.basic_serialize(),
+        }
+    )
+
+    return response, 201
+
+
+@projectBP.route("/<int:id>", methods=["DELETE"], strict_slashes=False)
+def delete_project(id):
+    project = Projects.query.filter_by(id=id).first()
+
+    if not project:
+        return jsonify({"success": False, "message": "Project not found"}), 404
+
+    db.session.delete(project)
+    db.session.commit()
+
+    response = jsonify({"success": True, "message": "Project successfully deleted"})
+
+    return response, 201
