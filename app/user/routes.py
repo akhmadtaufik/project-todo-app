22ffbd26e31db48ab9012e0116145a8bf97130ca
akhmadtaufik <akhmadtaufik1703@gmail.com>
2023-12-17 12:53:02 +0700

    feat: Add additional user CRUD operations

--- app/user/routes.py
+++ app/user/routes.py
@@ -47,3 +47,64 @@ def create_user():
     )
 
     return response, 201
+
+
+@userBP.route("/<int:id>", methods=["GET"], strict_slashes=False)
+def get_user_by_id(id):
+    user = Users.query.filter_by(id=id).first()
+
+    if not user:
+        return jsonify({"success": False, "message": "User not found"}), 404
+
+    else:
+        response = jsonify({"success": True, "data": user.serialize()})
+
+    return response, 200
+
+
+@userBP.route("/<int:id>", methods=["PUT"], strict_slashes=False)
+def update_user(id):
+    data = request.get_json()
+    input_username = data.get("username")
+    input_email = data.get("email")
+    input_password = data.get("password")
+
+    user = Users.query.filter_by(id=id).first()
+
+    if not user:
+        return jsonify({"success": False, "message": "User not found"}), 404
+
+    elif not input_username or not input_email or not input_password:
+        return jsonify({"succes": False, "message": "Data not complete"}), 422
+
+    else:
+        user.username = input_username
+        user.email = input_email
+        user.password = input_password
+
+    db.session.commit()
+
+    response = jsonify(
+        {
+            "success": True,
+            "message": "Data successfully updated",
+            "data": user.basic_serialize(),
+        }
+    )
+
+    return response, 201
+
+
+@userBP.route("/<int:id>", methods=["DELETE"], strict_slashes=False)
+def delete_user(id):
+    user = Users.query.filter_by(id=id).first()
+
+    if not user:
+        return jsonify({"success": False, "message": "User not found"}), 404
+    else:
+        db.session.delete(user)
+        db.session.commit()
+
+    response = jsonify({"success": True, "message": "User successfully deleted"})
+
+    return response, 201
