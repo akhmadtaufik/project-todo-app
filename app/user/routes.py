1ae7d133d99045a0164a507d2fd13f9133055bce
akhmadtaufik <akhmadtaufik1703@gmail.com>
2023-12-17 15:12:54 +0700

    refactor: Enhance structure, error handling, and response consistency in user routes

--- app/user/routes.py
+++ app/user/routes.py
@@ -7,16 +7,19 @@ from sqlalchemy.orm.exc import NoResultFound
 
 
 @userBP.route("/", methods=["GET"], strict_slashes=False)
-def get_all_user():
+def get_all_users():
     try:
+        # Parse the 'limit' query parameter, default to 10 if not provided
         limit = int(request.args.get("limit", 10))
     except ValueError:
-        return jsonify({"message": "Invalid Parameter"}), 422
+        # Return a 422 Unprocessable Entity response for invalid parameter
+        return jsonify({"error": "Invalid parameter"}), 422
 
+    # Query users and serialize the results
     users = db.session.query(Users).limit(limit).all()
-
     results = [user.serialize() for user in users]
 
+    # Construct a JSON response with the serialized user data
     response = jsonify({"success": True, "data": results})
 
     return response, 200
@@ -25,10 +28,12 @@ def get_all_user():
 @userBP.route("/<int:user_id>", methods=["GET"], strict_slashes=False)
 def get_user_by_id(user_id):
     try:
+        # Query user by ID or return a 404 Not Found response
         user = Users.query.get_or_404(user_id)
         response = jsonify({"success": True, "data": user.serialize()})
         return response, 200
     except NoResultFound:
+        # Return a 404 Not Found response if the user is not found
         return jsonify({"success": False, "message": "User not found"}), 404
 
 
@@ -44,11 +49,13 @@ def create_user():
 
     # Validate input data
     if not all((input_username, input_email, input_password)):
-        return jsonify({"message": "Incomplete data"}), 422
+        # Return a 422 Unprocessable Entity response for incomplete data
+        return jsonify({"error": "Incomplete data"}), 422
 
     # Check email uniqueness before adding to the session
     if is_email_taken(input_email):
-        return jsonify({"message": "Email already exists"}), 422
+        # Return a 422 Unprocessable Entity response for existing email
+        return jsonify({"error": "Email already exists"}), 422
 
     try:
         # Create a new user and add to the session
@@ -57,12 +64,12 @@ def create_user():
         )  # type: ignore
         db.session.add(new_user)
         db.session.commit()
-    except IntegrityError as e:
+    except IntegrityError:
+        # Handle email uniqueness constraint violation with a rollback
         db.session.rollback()
-        print(f"Error: {str(e)}")
-        return jsonify({"message": "Email already exists"}), 422
+        return jsonify({"error": "Email already exists"}), 422
 
-    # Return success response
+    # Return success response with the newly created user data
     response = jsonify(
         {
             "success": True,
@@ -79,8 +86,8 @@ def is_email_taken(email):
     return Users.query.filter_by(email=email).first() is not None
 
 
-@userBP.route("/<int:id>", methods=["PUT"], strict_slashes=False)
-def update_user(id):
+@userBP.route("/<int:user_id>", methods=["PUT"], strict_slashes=False)
+def update_user(user_id):
     # Extract JSON data from the request
     data = request.get_json()
     username = data.get("username")
@@ -88,25 +95,25 @@ def update_user(id):
     password = data.get("password")
 
     try:
-        # Query the user based on ID
-        user = Users.query.filter_by(id=id).first()
+        # Query the user based on ID or return a 404 Not Found response
+        user = Users.query.get_or_404(user_id)
     except SQLAlchemyError:
-        # Handle database error
-        return jsonify({"success": False, "message": "Database error"}), 500
+        # Handle database error with a 500 Internal Server Error response
+        return jsonify({"error": "Database error"}), 500
 
     if not user:
-        # If user is not found, return a 404 response
-        return jsonify({"success": False, "message": "User not found"}), 404
+        # If user is not found, return a 404 Not Found response
+        return jsonify({"error": "User not found"}), 404
 
     if not all((username, email, password)):
-        # If data is incomplete, return a 422 response
-        return jsonify({"success": False, "message": "Data not complete"}), 422
+        # If data is incomplete, return a 422 Unprocessable Entity response
+        return jsonify({"error": "Data not complete"}), 422
 
-    # Update the user object with new data
+    # Update the user object with new data and commit the transaction
     update_user_object(user, username, email, password)
     db.session.commit()
 
-    # Construct a JSON response with the newly updated data
+    # Construct a JSON response with the newly updated user data
     response = jsonify(
         {
             "success": True,
@@ -115,7 +122,7 @@ def update_user(id):
         }
     )
 
-    return response, 201
+    return response, 200
 
 
 def update_user_object(user, username, email, password):
@@ -125,15 +132,11 @@ def update_user_object(user, username, email, password):
     user.password = password
 
 
-@userBP.route("/<int:id>", methods=["DELETE"], strict_slashes=False)
-def delete_user(id):
+@userBP.route("/<int:user_id>", methods=["DELETE"], strict_slashes=False)
+def delete_user(user_id):
     try:
-        # Query the user based on ID
-        user = Users.query.filter_by(id=id).first()
-
-        if not user:
-            # If user is not found, return a 404 response
-            return jsonify({"success": False, "message": "User not found"}), 404
+        # Query the user based on ID or return a 404 Not Found response
+        user = Users.query.get_or_404(user_id)
 
         # Delete the user from the database and commit the transaction
         db.session.delete(user)
@@ -142,7 +145,7 @@ def delete_user(id):
         # Construct a JSON response for a successful deletion with status code 204 No Content
         response = jsonify({"success": True, "message": "User successfully deleted"})
         return response, 204
-    except Exception:
-        # Handle database error, perform a rollback, and return a 500 response
+    except SQLAlchemyError:
+        # Handle database error, perform a rollback, and return a 500 Internal Server Error response
         db.session.rollback()
-        return jsonify({"success": False, "message": "Error deleting user"}), 500
+        return jsonify({"error": "Error deleting user"}), 500
