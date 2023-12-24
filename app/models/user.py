792851aa59a6a02cbc6f97302099a385e0578dcf
akhmadtaufik <akhmadtaufik1703@gmail.com>
2023-12-24 17:11:35 +0700

    refactor: Rename 'username' to 'name' in Users model

--- app/models/user.py
+++ app/models/user.py
@@ -4,8 +4,8 @@ from app.extensions import db
 class Users(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
+    name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
     password = db.Column(db.String(1024), nullable=False)
     created_at = db.Column(db.TIMESTAMP, server_default=db.func.now())
     update_at = db.Column(
@@ -19,7 +19,7 @@ class Users(db.Model):
     def serialize(self):
         return {
             "user_id": self.id,
-            "username": self.username,
+            "name": self.name,
             "email": self.email,
             "created_at": str(self.created_at),
             "update_at": str(self.update_at),
@@ -28,7 +28,7 @@ class Users(db.Model):
 
     def basic_serialize(self):
         return {
-            "username": self.username,
+            "username": self.name,
             "email": self.email,
             "update_at": str(self.update_at),
         }
