17fbc6001a83c9bfb07271a0be9d63479802be67
akhmadtaufik <akhmadtaufik1703@gmail.com>
2023-12-24 17:02:37 +0700

    feat: Integrate Flask-JWT-Extended for JSON Web Token authentication

--- app/extensions.py
+++ app/extensions.py
@@ -1,5 +1,13 @@
+from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+# Initializing Flask-SQLAlchemy for database management.
 db = SQLAlchemy()
+
+# Initializing Flask-Migrate for handling database migrations.
 migrate = Migrate()
+
+# Initializing Flask-JWT-Extended for implementing
+# JSON Web Token (JWT) based authentication.
+jwt = JWTManager()
